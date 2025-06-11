f04f0e0c6d4a92ee316b3fab65a326fc019147ee
picspiii1234 <ibrihim539@gmail.com>
2025-06-11 09:55:08 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,83 @@
+// function test(){
+//     let son1 =20;
+//     let son2 =30;
+//     return son1 + son2;
+// }
+
+
+// console.log(test());
+
+
+// let son1 =prompt('son kiriting')
+// let son2 =prompt('kopaytirish kerak bolgan soni kiriting')
+
+// function test(){
+//      return son1 * son2;
+// }
+
+
+
+// console.log(test());
+
+
+// const body =document.querySelector('body')
+
+// function dark(){
+//     console.log(dark());
+//     body.style.backgroundColor = 'black'
+// }
+
+const header = document.querySelector('header')
+
+header.style.padding= '50px 120px'
+
+//---------------------------------------------------
+
+
+const container = document.getElementById('container')
+
+container.style.display = 'flex';
+container.style.alignItems = 'center';
+container.style.justifyContent= 'space-between'
+
+//----------------------------------------------------
+
+
+
+const container1 = document.getElementById('container1')
+
+container1.style.display = 'flex'
+container1.style.gap = '20px'
+
+//------------------------------------------------------
+
+
+const button = document.querySelector('button')
+
+button.style.width = '140px'
+button.style.height = '40px'
+button.style.backgroundColor = ('rgb(251, 152, 47)')
+button.style.color = 'black'
+button.style.border = 'none'
+button.style.borderRadius = '6px'
+
+
+//------------------------------------------------------
+
+
+const box2 = document.getElementById('box2')
+
+box2.style.width = '140px'
+box2.style.height = '40px'
+box2.style.backgroundColor = 'black'
+box2.style.color = 'white'
+box2.style.borderRadius = '6px'
+
+
+//-------------------------------------------------------
+
+
+if(button){
+body.style.backgroundColor = 'rgb(40, 39, 39);'
+
+}
